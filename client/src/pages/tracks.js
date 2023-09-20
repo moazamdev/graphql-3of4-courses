f8cd918c75a43cfaf5df5781420ecdd1fe417f6a
Moazam Ali <mozalikorai@gmail.com>
2023-09-20 08:03:23 -0700

    added modules feature

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import TrackCard from '../containers/track-card';
-import { Layout, QueryResult } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Layout, QueryResult } from "../components";
+import { Link } from "react-router-dom";
 
 /** TRACKS gql query to retrieve all tracks */
 const TRACKS = gql`
-  query getTracks {
-    tracksForHome {
-      id
-      title
-      thumbnail
-      length
-      modulesCount
-      author {
-        name
-        photo
-      }
-    }
-  }
+	query getTracks {
+		tracksForHome {
+			id
+			title
+			thumbnail
+			length
+			modulesCount
+			author {
+				name
+				photo
+			}
+		}
+	}
 `;
 
 /**
@@ -25,17 +26,17 @@ const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  const { loading, error, data } = useQuery(TRACKS);
+	const { loading, error, data } = useQuery(TRACKS);
 
-  return (
-    <Layout grid>
-      <QueryResult error={error} loading={loading} data={data}>
-        {data?.tracksForHome?.map((track, index) => (
-          <TrackCard key={track.id} track={track} />
-        ))}
-      </QueryResult>
-    </Layout>
-  );
+	return (
+		<Layout grid>
+			<QueryResult error={error} loading={loading} data={data}>
+				{data?.tracksForHome?.map((track, index) => (
+					<TrackCard key={track.id} track={track} />
+				))}
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Tracks;
